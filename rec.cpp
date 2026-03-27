708d2c3bad684d4c22cc36dd714aa5d168c54ca5
Shurik01 <128497231+Shurik01@users.noreply.github.com>
2026-03-27 09:48:18 +0900

    измерение времени, исправлены ошибки функции selectionsort_rec

--- rec.cpp
+++ rec.cpp
@@ -1,16 +1,104 @@
 #include <iostream>
 #include <algorithm> 
+#include "function.hpp"
+#include <cassert>
+#include <chrono>
 using namespace std;
+/// @brief функция сортировки массива простым выбором(итеративная)
+/// @param arr массив
+/// @param n количество чисел в массиве
+/// @return отсортированный массив
 int* selectionSort(int* arr, size_t n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
+    for (size_t i = 0; i < n - 1; i++) {
+        size_t min_idx = i;
+        for (size_t j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
-        // Меняем местами
+        // Меняет местами элементы массива
         swap(arr[i], arr[min_idx]);
     }
     return arr;
+}
+
+/// @brief функция сортировки массива простым выбором(рекурсивная)
+/// @param arr массив
+/// @param n количество чисел в массиве
+/// @return отсортированный массив
+int* selectionSort_rec(int* arr, size_t n, size_t idx = 0) {
+    if (idx == n-1){
+        return arr;
+    }
+    size_t min_idx = idx;
+    for (size_t i = idx + 1; i < n; i++){
+        if (arr[i] < arr[min_idx]){
+            min_idx = i;
+        }
+    }
+    swap(arr[idx], arr[min_idx]);
+    return selectionSort_rec(arr, n, idx + 1);
+}
+
+
+int main(){
+    // размер массива
+    size_t n = 20;
+
+    // выделение памяти под массивы
+    int* arr = new int[n];
+    int* arr2 = new int[n];
+    int* arr3 = new int[n];
+    // заполнение массивов случайными числами
+    arr = random_int_array(arr, n, -100, 100);
+    arr2 = random_int_array(arr, n, -1000, 1000);
+    arr3 = random_int_array(arr, n, -10000, 10000);
+    
+    // тест функции selectionSort
+    assert(is_arr_sorted(selectionSort(arr, n), n) == 1);
+    assert(is_arr_sorted(selectionSort(arr2, n), n) == 1);
+    assert(is_arr_sorted(selectionSort(arr3, n), n) == 1);
+    arr = random_int_array(arr, n, -100, 100);
+    arr2 = random_int_array(arr, n, -1000, 1000);
+    arr3 = random_int_array(arr, n, -10000, 10000);
+    // тест функции selectionSort_rec
+    assert(is_arr_sorted(selectionSort_rec(arr, n), n) == 1);
+    assert(is_arr_sorted(selectionSort_rec(arr2, n), n) == 1);
+    assert(is_arr_sorted(selectionSort_rec(arr3, n), n) == 1);
+    // Переменная для суммирования времени в наносекундах
+    long long total_nanoseconds = 0;
+    // начальная отметка времени
+    auto t0 = steady_clock::now();
+    // код, время выполнения которого нужно измерить
+    for (size_t i = 0; i < 10'000; i++){
+        arr = random_int_array(arr, n, -100, 100);
+        selectionSort(arr, n);
+        
+    }
+    // конечная отметка времени
+    auto t1 = steady_clock::now();
+    total_nanoseconds += duration_cast<nanoseconds>(t1 - t0).count();
+    double total_milliseconds = total_nanoseconds / 1'000'000.0;
+    
+    cout << "Время выполнения функции selectionSort: " << total_milliseconds << " ms" << endl;
+    total_nanoseconds = 0;
+    // начальная отметка времени
+    t0 = steady_clock::now();
+    // код, время выполнения которого нужно измерить
+    for (size_t i = 0; i < 10'000; i++){
+        arr = random_int_array(arr, n, -100, 100);
+        selectionSort_rec(arr, n);
+        
+    }
+    // конечная отметка времени
+    t1 = steady_clock::now();
+    total_nanoseconds += duration_cast<nanoseconds>(t1 - t0).count();
+    total_milliseconds = total_nanoseconds / 1'000'000.0;
+    
+    cout << "Время выполнения функции selectionSort_rec: " << total_milliseconds << " ms" << endl;
+
+    delete[] arr;
+    delete[] arr2;
+    delete[] arr3;
+    return 0;
 }
