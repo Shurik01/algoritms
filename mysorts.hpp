34a72b88b74a7af56d12213a480cdd003cffb98c
Shurik01 <128497231+Shurik01@users.noreply.github.com>
2026-04-11 11:01:51 +0900

    все функции шаблонные

--- mysorts.hpp
+++ mysorts.hpp
@@ -24,13 +24,23 @@ int* selectionSort(T* arr, size_t n) {
     return arr;
 }
 
-
-
 /// @brief функция сортировки массива простым выбором(рекурсивная)
 /// @param arr массив
 /// @param n количество чисел в массиве
 /// @return отсортированный массив
-int* selectionSort_rec(int* arr, size_t n, size_t idx = 0);
+int* selectionSort_rec(int* arr, size_t n, size_t idx = 0) {
+    if (idx == n-1){
+        return arr;
+    }
+    size_t min_idx = idx;
+    for (size_t i = idx + 1; i < n; i++){
+        if (arr[i] < arr[min_idx]){
+            min_idx = i;
+        }
+    }
+    swap(arr[idx], arr[min_idx]);
+    return selectionSort_rec(arr, n, idx + 1);
+}
 
 /// @brief разбиение массива по схеме Ломуто
 /// @tparam T шаблон типа массива
